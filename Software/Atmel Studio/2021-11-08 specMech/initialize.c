9caf39845fc72d752852d6796ff71f8fa4f692aa
Alan Uomoto <auomoto@gmail.com>
2021-11-10 08:06:51 -0800

    Added to office GitHub repository

---
+++ Software/Atmel Studio/2021-11-08 specMech/initialize.c
@@ -0,0 +1,77 @@
+#include "globals.h"
+#include "oled.h"
+#include "eeprom.h"
+#include "roboclaw.h"
+#include "initialize.h"
+
+uint8_t rebootackd;
+
+/*
+void initialize(void)
+{
+
+	char versionstr[11];
+
+	init_PORTS();
+	PORTF.PIN6CTRL = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;	// Pushbutton setup
+	init_XPORT();
+	init_SPECID();
+	init_LED();
+	init_BEEPER();
+	init_TWI();
+	init_RTC(32);	// Fast, 1/16 sec, for blinking LED at startup
+	init_USART();
+
+	init_ROBO();
+	init_MMA8451();	// requires TWI interrupts
+	init_PNEU();	// requires TWI interrupts
+	init_OLED(0);
+	init_OLED(1);
+	init_EEPROM();	// Needs TWI for get_time()
+	timeoutOLED = 30 * (512/32);	// 30 seconds for version display
+	get_VERSION(versionstr);
+	writestr_OLED(1,"specMech Version", 1);
+	writestr_OLED(1, versionstr, 2);
+
+}
+*/
+
+void initialize0(void)
+{
+
+	init_PORTS();
+	PORTF.PIN6CTRL = PORT_PULLUPEN_bm | PORT_ISC_FALLING_gc;	// Pushbutton setup
+	init_XPORT();
+	init_SPECID();
+	init_LED();
+	init_BEEPER();
+	init_TWI();
+	init_RTC(32);	// Fast, 1/16 sec, for blinking LED at startup
+	init_USART();
+
+}
+
+void initialize1(void)
+{
+
+	char versionstr[11];
+
+	rebootackd = NO;
+//	rebootackd = YES;
+	init_MOTORS();
+	init_MMA8451();
+	init_PNEU();
+	reset_OLED();
+	init_OLED(0);
+	init_OLED(1);
+	_delay_ms(100);				// Wait after display-on command
+	init_EEPROM();	// Needs TWI
+	timerOLED = 0;
+	timeoutOLED = 30 * (512/32);	// 30 seconds for version display
+	get_VERSION(versionstr);
+	writestr_OLED(1,"specMech Version", 1);
+	writestr_OLED(1, versionstr, 2);
+
+}
+
+
---
+++ Software/Atmel Studio/2021-11-08 specMech/oled.h
@@ -0,0 +1,20 @@
+#ifndef OLEDH
+#define OLEDH
+
+#define OLEDADDR0		(0x3C)		// TWI bus address
+#define OLEDADDR1		(0x3D)		// TWI bus address
+#define CLEARDISPLAY	0x01		// Newhaven command (not used)
+#define DISPLAYON		0x0C		// Newhaven command (not used)
+#define DISPLAYOFF		0x08		// Newhaven command (not used)
+#define OLEDCMD			0x00		// Newhaven command was 1
+#define OLEDDATA		0x40		// Newhaven command was 0
+#define OLEDLINE1		0x80		// Newhaven command
+#define OLEDLINE2		0xC0		// Newhaven command
+
+void clear_OLED(uint8_t);
+void reset_OLED(void);
+void write_OLED(uint8_t, uint8_t, uint8_t);
+void writestr_OLED(uint8_t, char*, uint8_t);
+extern uint16_t timerOLED, timeoutOLED;	// Used to blank the display
+
+#endif
